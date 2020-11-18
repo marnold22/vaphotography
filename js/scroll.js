ecef1b07fd397f416ca5b10232d1e1634e6c2a48
Michael <michaelarnold615@gmail.com>
2020-11-18 15:10:18 -0800

    Updated scroll js to include the navbar color change

--- js/scroll.js
+++ js/scroll.js
@@ -1,25 +1,37 @@
 // Fluid scroll to about anchor
 function aboutScroll() {
-    var element = document.querySelector("#about");
-    element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+  var element = document.querySelector("#about");
+  element.scrollIntoView({ behavior: "smooth", block: "start" });
 }
 // Fluid scroll to portfolio anchor
 function portfolioScroll() {
-    var element = document.querySelector("#portfolio");
-    element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+  var element = document.querySelector("#portfolio");
+  element.scrollIntoView({ behavior: "smooth", block: "start" });
 }
 // Fluid scroll to contact anchor
 function contactScroll() {
-    var element = document.querySelector("#contact");
-    element.scrollIntoView({ behavior: 'smooth', block: 'start'});
+  var element = document.querySelector("#contact");
+  element.scrollIntoView({ behavior: "smooth", block: "start" });
 }
 
 // Used to toggle the menu on small screens when clicking on the menu button
 function toggleFunction() {
-    var x = document.getElementById("navMobile");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
-    } else {
-      x.className = x.className.replace(" w3-show", "");
-    }
+  var x = document.getElementById("navMobile");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
   }
+}
+
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+
+  if (scroll >= 600) {
+    $("#navDesktop").addClass("navScrolled");
+    $("#navDesktop").addClass("w3-card-4");
+  } else {
+    $("#navDesktop").removeClass("navScrolled");
+    $("#navDesktop").removeClass("w3-card-4");
+  }
+});
