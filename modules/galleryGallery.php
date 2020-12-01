721e65c440c4e54efe759a5c300c583550352e37
Michael <michaelarnold615@gmail.com>
2020-12-01 13:17:08 -0800

    Created gallery modules (contains image cards)

---
+++ modules/galleryGallery.php
@@ -0,0 +1,83 @@
+<!-- Row 1 of Cards -->
+<div class="w3-row-padding w3-margin-top">
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <img class="imgcard" src="assets/gallery/flowers/Flower-4.png" onclick="galleryDisplay(this)">
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <img class="imgcard" src="assets/gallery/flowers/Flower-11.png" onclick="galleryDisplay(this)">
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <img class="imgcard" src="assets/gallery/flowers/Flower-17.png" onclick="galleryDisplay(this)">
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+</div>
+
+<!-- Row 2 of Cards -->
+<div class="w3-row-padding w3-margin-top">
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <img class="imgcard" src="assets/gallery/landscape/Landscape-16.png" onclick="galleryDisplay(this)">
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <img class="imgcard" src="assets/gallery/landscape/Landscape-14.png" onclick="galleryDisplay(this)">
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <img class="imgcard" src="assets/gallery/landscape/Landscape-13.png" onclick="galleryDisplay(this)">
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+</div>
+
+<!-- Row 3 of Cards -->
+<div class="w3-row-padding w3-margin-top">
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <img class="imgcard" src="assets/gallery/other/Other-1.png" onclick="galleryDisplay(this)">
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <img class="imgcard" src="assets/gallery/other/Other-4.png" onclick="galleryDisplay(this)">
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <img class="imgcard" src="assets/gallery/other/Other-9.png" onclick="galleryDisplay(this)">
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+</div>
---
+++ modules/homeGallery.php
@@ -0,0 +1,56 @@
+<!-- Row 1 of Cards -->
+<div class="w3-row-padding w3-margin-top">
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <!-- <img class="imgcard" src="assets/gallery/flowers/Flower-4.png" onclick="indexDisplay(this)"> -->
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <!-- <img class="imgcard" src="assets/gallery/flowers/Flower-11.png" onclick="indexDisplay(this)"> -->
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <!-- <img class="imgcard" src="assets/gallery/flowers/Flower-17.png" onclick="indexDisplay(this)"> -->
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+</div>
+
+
+<!-- Row 2 of Cards -->
+<div class="w3-row-padding w3-margin-top">
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <!-- <img class="imgcard" src="assets/gallery/landscape/Landscape-16.png" onclick="indexDisplay(this)"> -->
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <!-- <img class="imgcard" src="assets/gallery/landscape/Landscape-14.png" onclick="indexDisplay(this)"> -->
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+    <div class="w3-third">
+    <div class="w3-card cardhover">
+        <!-- <img class="imgcard" src="assets/gallery/landscape/Landscape-13.png" onclick="indexDisplay(this)"> -->
+        <div class="w3-container">
+        <h5>5 Terre</h5>
+        </div>
+    </div>
+    </div>
+</div>
