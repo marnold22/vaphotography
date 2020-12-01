0c16f8bd398e34c2f73e82c09180419b174dad45
Michael <michaelarnold615@gmail.com>
2020-12-01 13:17:31 -0800

    Linked gallery modules to their respective pages

--- gallery.php
+++ gallery.php
@@ -18,91 +18,10 @@
     <p class="w3-center"><em>Here are some of my latest pieces of work!<br> Click on the images to make them larger</em>
     </p><br>
 
-    <!-- Row 1 of Cards -->
-    <div class="w3-row-padding w3-margin-top">
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <img class="imgcard" src="assets/gallery/flowers/Flower-4.png" onclick="galleryDisplay(this)">
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <img class="imgcard" src="assets/gallery/flowers/Flower-11.png" onclick="galleryDisplay(this)">
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <img class="imgcard" src="assets/gallery/flowers/Flower-17.png" onclick="galleryDisplay(this)">
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-    </div>
+    <!-- INCLUDE GALLERY -->
+    <?php include 'modules/galleryGallery.php'; ?>
 
-    <!-- Row 2 of Cards -->
-    <div class="w3-row-padding w3-margin-top">
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <img class="imgcard" src="assets/gallery/landscape/Landscape-16.png" onclick="galleryDisplay(this)">
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <img class="imgcard" src="assets/gallery/landscape/Landscape-14.png" onclick="galleryDisplay(this)">
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <img class="imgcard" src="assets/gallery/landscape/Landscape-13.png" onclick="galleryDisplay(this)">
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Row 3 of Cards -->
-    <div class="w3-row-padding w3-margin-top">
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <img class="imgcard" src="assets/gallery/other/Other-1.png" onclick="galleryDisplay(this)">
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <img class="imgcard" src="assets/gallery/other/Other-4.png" onclick="galleryDisplay(this)">
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <img class="imgcard" src="assets/gallery/other/Other-9.png" onclick="galleryDisplay(this)">
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Gallery Container End -->
+    <!-- GALLERY END -->
   </div>
 
   <!-- INCLUDE FOOTER -->
--- index.php
+++ index.php
@@ -64,61 +64,8 @@
     <p class="w3-center"><em>Here are some of my latest pieces of work!<br> Click on the images to make them larger</em>
     </p><br>
 
-    <!-- Row 1 of Cards -->
-    <div class="w3-row-padding w3-margin-top">
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <!-- <img class="imgcard" src="assets/gallery/flowers/Flower-4.png" onclick="indexDisplay(this)"> -->
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <!-- <img class="imgcard" src="assets/gallery/flowers/Flower-11.png" onclick="indexDisplay(this)"> -->
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <!-- <img class="imgcard" src="assets/gallery/flowers/Flower-17.png" onclick="indexDisplay(this)"> -->
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <!-- Row 2 of Cards -->
-    <div class="w3-row-padding w3-margin-top">
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <!-- <img class="imgcard" src="assets/gallery/landscape/Landscape-16.png" onclick="indexDisplay(this)"> -->
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <!-- <img class="imgcard" src="assets/gallery/landscape/Landscape-14.png" onclick="indexDisplay(this)"> -->
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-      <div class="w3-third">
-        <div class="w3-card cardhover">
-          <!-- <img class="imgcard" src="assets/gallery/landscape/Landscape-13.png" onclick="indexDisplay(this)"> -->
-          <div class="w3-container">
-            <h5>5 Terre</h5>
-          </div>
-        </div>
-      </div>
-    </div>
+    <!-- INCLUDE GALLERY -->
+    <?php include 'modules/homeGallery.php'; ?>
 
     <!-- Button for more images -->
     <div class="w3-row-padding w3-center w3-section">
