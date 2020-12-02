50529caf9410824d736c39acf283b21afe04d8b5
Michael <michaelarnold615@gmail.com>
2020-12-02 11:03:55 -0800

    Fixed galleryNavbar to use index.php not .html

--- modules/galleryNav.php
+++ modules/galleryNav.php
@@ -5,12 +5,12 @@
         title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
       <a class="w3-hide-medium w3-hide-large" onClick="window.scroll({ top: 0,left: 0,behavior: 'smooth' });"><img
           class="logo-mobile" src="/assets/logo/VAP-Logo-White.png"></a>
-      <a href="index.html" class="w3-bar-item w3-hide-small"><img class="logo"
+      <a href="index.php" class="w3-bar-item w3-hide-small"><img class="logo"
           src="/assets/logo/VAP-Logo-White.png"></a>
     </div>
 
     <!-- Navbar on mobile screens -->
     <div id="navMobile" class="w3-bar-block w3-dark-grey w3-hide w3-hide-large w3-hide-medium">
-      <a href="index.html" class="w3-bar-item w3-button">HOME</a>
+      <a href="index.php" class="w3-bar-item w3-button">HOME</a>
     </div>
   </div>
