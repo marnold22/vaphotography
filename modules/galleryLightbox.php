ccde7b34df19f9c31948e9cff179d9d9638ad328
Michael <michaelarnold615@gmail.com>
2021-11-11 13:04:44 -0800

    Removed modules
    These modules added extra http requests. So I combined them into their respective files (ie. index & gallery)

--- modules/galleryLightbox.php
+++
@@ -1,8 +0,0 @@
-  <!-- Modal for full size images on click-->
-  <div id="gallery-modal" class="w3-modal w3-black" style="padding-top:10%" onclick="this.style.display='none'">
-    <span class="w3-button w3-black w3-xlarge w3-display-topright">×</span>
-    <div class="w3-modal-content w3-animate-zoom w3-center w3-transparent w3-padding-64">
-      <img id="img01" class="w3-image">
-      <p id="caption"></p>
-    </div>
-  </div>
