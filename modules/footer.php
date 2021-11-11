b05316e9b72794354bd5a301a48f40b3f2615037
Michael <michaelarnold615@gmail.com>
2021-11-11 13:09:14 -0800

    Fixed footer to auto correct the date
    Also added the dark grey color

--- modules/footer.php
+++ modules/footer.php
@@ -1,5 +1,9 @@
-<footer class="w3-center w3-black w3-padding-64 w3-opacity">
-<div class="w3-xlarge w3-section">
-</div>
-<p>&copy VAPhotography 2021</p>
+<footer class="w3-center w3-padding-64 bg-grey">
+    <div class="w3-xlarge w3-section"></div>
+    <p>
+        &copy VAPhotography
+        <script>
+            document.write(new Date().getFullYear())
+        </script>
+    </p>
 </footer>
