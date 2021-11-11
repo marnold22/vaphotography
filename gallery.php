434cc06486439cd297dec4d3a63fc941efac69d8
Michael <michaelarnold615@gmail.com>
2021-11-11 13:10:45 -0800

    Overhaul of index and gallery
    Updated the home page and the gallery to use the new dark grey color (navbar and other areas).
    
    Also merged "module" files into these main pages to reduce the number of http requests

--- gallery.php
+++ gallery.php
@@ -5,15 +5,36 @@
 <?php include 'modules/header.php' ?>
 
 <body>
-<!-- INCLUDE NAVBAR -->
-<?php include 'modules/galleryNav.php' ?>
+  <!-- Navbar (sit on top) -->
+  <div class="w3-top">
+    <div id="navGallery" class="w3-bar w3-large bg-grey">
+      <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" onclick="toggleMobileMenu()"
+        title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+      <a class="w3-hide-medium w3-hide-large" onClick="window.scroll({ top: 0,left: 0,behavior: 'smooth' });"><img
+          class="logo-mobile" src="/assets/logo/VAP-Logo-White.png"></a>
+      <a href="index.php" class="w3-bar-item w3-hide-small"><img class="logo"
+          src="/assets/logo/VAP-Logo-White.png"></a>
+    </div>
 
-<!-- INCLUDE LIGHTBOX -->
-<?php include 'modules/galleryLightbox.php' ?>
+    <!-- Navbar on mobile screens -->
+    <div id="navMobile" class="w3-bar-block bg-grey w3-hide w3-hide-large w3-hide-medium">
+      <a href="index.php" class="w3-bar-item w3-button">HOME</a>
+    </div>
+  </div>
+
+  <!-- INCLUDE LIGHTBOX -->
+  <!-- Modal for full size images on click-->
+  <div id="gallery-modal" class="w3-modal w3-black" style="padding-top:10%" onclick="this.style.display='none'">
+    <span class="w3-button w3-black w3-xlarge w3-display-topright">×</span>
+    <div class="w3-modal-content w3-animate-zoom w3-center w3-transparent w3-padding-64">
+      <img id="img01" class="w3-image">
+      <p id="caption"></p>
+    </div>
+  </div>
 
 
   <!-- GALLERY START -->
-  <div class="w3-padding-64">
+  <div class="w3-padding-64 fc-dark">
     <h1 class="w3-center">MY GALLERY</h1>
     <p class="w3-center"><em>Here are some of my latest pieces of work!<br> Click on the images to make them larger</em>
     </p><br>
@@ -25,7 +46,13 @@
   </div>
 
   <!-- INCLUDE FOOTER -->
-  <?php include 'modules/footer.php';?>
+  <footer class="w3-center w3-padding-64 bg-grey">
+    <div class="w3-xlarge w3-section"></div>
+    <p>
+      &copy VAPhotography 
+      <script>document.write(new Date().getFullYear())</script>
+    </p>
+  </footer>
   
   <script src="js/gallery.js"></script>
 </body>
--- index.php
+++ index.php
@@ -6,11 +6,24 @@
 
 <body>
 
-  <!-- INCLUDE NAVBAR -->
-  <?php include 'modules/homeNav.php' ?>
+  <!-- Navbar for desktop and tablet -->
+  <div class="w3-top">
+    <div id="navHome" class="w3-bar w3-large bg-grey navBackground">
+      <a class="w3-bar-item w3-button w3-hover-black w3-hide-medium w3-hide-large w3-right" onclick="toggleMobileMenu()" title="Toggle Navigation Menu"><i class="fa fa-bars"></i></a>
+      <a class="w3-hide-medium w3-hide-large" onClick="window.scroll({ top: 0,left: 0,behavior: 'smooth' });"><img class="logo-mobile" src="/assets/logo/VAP-Logo-White.png"></a>
+      <a class="w3-bar-item w3-hide-small w3-right" onclick="contactScroll()">CONTACT</a>
+      <a class="w3-bar-item w3-hide-small w3-right" onclick="portfolioScroll()">GALLERY</a>
+      <a class="w3-bar-item w3-hide-small w3-right" onclick="aboutScroll()">ABOUT</a>
+      <a class="w3-bar-item w3-hide-small" onClick="window.scroll({ top: 0,left: 0,behavior: 'smooth' });"><img class="logo" src="/assets/logo/VAP-Logo-White.png"></a>
+    </div>
 
-
-  <!-- First Parallax Image with Logo Text -->
+    <!-- Navbar on mobile screens -->
+    <div id="navMobile" class="w3-bar-block w3-hide w3-hide-large w3-hide-medium bg-grey">
+      <a class="w3-bar-item w3-button" onclick="aboutMobile()">ABOUT</a>
+      <a class="w3-bar-item w3-button" onclick="portfolioMobile()">GALLERY</a>
+      <a class="w3-bar-item w3-button" onclick="contactMobile()">CONTACT</a>
+    </div>
+  </div <!-- First Parallax Image with Logo Text -->
   <div class="bgimg-1 w3-display-container w3-opacity-min" id="home">
     <div class="w3-display-middle" style="white-space:nowrap;">
       <span id="va-banner" class="w3-center w3-padding-large w3-black w3-wide w3-animate-opacity">VA_PHOTOGRAPHY</span>
@@ -18,7 +31,7 @@
   </div>
 
   <!-- ABOUT SECTION -->
-  <div id="about" class="w3-content w3-container w3-padding-64">
+  <div id="about" class="w3-content w3-container w3-padding-64 fc-dark">
     <!-- <h3 class="w3-center">ABOUT ME</h3> -->
     <p class="w3-center w3-xxlarge quote">
       <span style="font-size:100px">&#10077;</span><br>
@@ -27,7 +40,7 @@
     <p></p>
   </div>
 
-  <div class="w3-row w3-center w3-dark-grey w3-padding-16">
+  <div class="w3-row w3-center w3-padding-16 bg-grey">
     <div class="w3-quarter w3-section">
       <span class="w3-xlarge">Nature</span><br>
     </div>
@@ -49,23 +62,94 @@
     </div>
   </div>
 
-  <!-- INCLUDE LIGHTBOX -->
-  <?php include 'modules/homeLightbox.php' ?>
+  <!-- INCLUDE LIGHTBOX (Modal for full size images on click) -->
+  <div id="index-modal" class="w3-modal w3-black" style="padding-top:0" onclick="this.style.display='none'">
+    <span class="w3-button w3-black w3-xlarge w3-display-topright">×</span>
+    <div class="w3-modal-content w3-animate-zoom w3-center w3-transparent w3-padding-64">
+      <img id="img01" class="w3-image">
+      <p id="caption"></p>
+    </div>
+  </div>
 
   <!-- PORTFOLIO SECTION -->
-  <div id="portfolio" class="w3-padding-64">
+  <div id="portfolio" class="w3-padding-64 fc-dark">
     <h3 class="w3-center">MY WORK</h3>
     <p class="w3-center"><em>Here are some of my latest pieces of work!<br> Click on the images to make them larger</em>
     </p><br>
 
-    <!-- INCLUDE GALLERY -->
-    <?php include 'modules/homeGallery.php'; ?>
+  <!-- INCLUDE GALLERY -->
+  <!-- Row 1 of Cards -->
+  <div class="w3-row-padding w3-margin-top">
+      <div class="w3-quarter w3-margin-top">
+      <div class="w3-card cardhover">
+          <img class="imgcard" src="assets/gallery/Flower-2.jpg" onclick="indexDisplay(this)">
+          <div class="w3-container">
+          </div>
+      </div>
+      </div>
+      <div class="w3-quarter w3-margin-top">
+      <div class="w3-card cardhover">
+          <img class="imgcard" src="assets/gallery/Landscape-8.jpg" onclick="indexDisplay(this)">
+          <div class="w3-container">
+          </div>
+      </div>
+      </div>
+      <div class="w3-quarter w3-margin-top">
+      <div class="w3-card cardhover">
+          <img class="imgcard" src="assets/gallery/Flower-11.jpg" onclick="indexDisplay(this)">
+          <div class="w3-container">
+          </div>
+      </div>
+      </div>
+      <div class="w3-quarter w3-margin-top">
+      <div class="w3-card cardhover">
+          <img class="imgcard" src="assets/gallery/Other-3.jpg" onclick="indexDisplay(this)">
+          <div class="w3-container">
+          </div>
+      </div>
+      </div>
+  </div>
 
-    <!-- Button for more images -->
-    <div class="w3-row-padding w3-center w3-section">
-      <a href="/gallery.php"><button class="w3-button w3-padding-large w3-light-grey" style="margin-top:64px">LOAD
-          MORE</button></a>
-    </div>
+  <!-- Row 2 of Cards -->
+  <div class="w3-row-padding w3-margin-top">
+      <div class="w3-quarter w3-margin-top">
+      <div class="w3-card cardhover">
+          <img class="imgcard" src="assets/gallery/Flower-7.jpg" onclick="indexDisplay(this)">
+          <div class="w3-container">
+          </div>
+      </div>
+      </div>
+      <div class="w3-quarter w3-margin-top">
+      <div class="w3-card cardhover">
+          <img class="imgcard" src="assets/gallery/Landscape-13.jpg" onclick="indexDisplay(this)">
+          <div class="w3-container">
+          </div>
+      </div>
+      </div>
+      <div class="w3-quarter w3-margin-top">
+      <div class="w3-card cardhover">
+          <img class="imgcard" src="assets/gallery/Landscape-15.jpg" onclick="indexDisplay(this)">
+          <div class="w3-container">
+          </div>
+      </div>
+      </div>
+      <div class="w3-quarter w3-margin-top">
+      <div class="w3-card cardhover">
+          <img class="imgcard" src="assets/gallery/Other-1.jpg" onclick="indexDisplay(this)">
+          <div class="w3-container">
+          </div>
+      </div>
+      </div>
+  </div>
+
+  <!-- Button for more images -->
+  <div class="w3-row-padding w3-center w3-section">
+    <a href="/gallery.php">
+      <button class="custom-btn" style="margin-top:64px">
+        LOAD MORE
+      </button>
+    </a>
+  </div>
 
   </div>
 
@@ -79,26 +163,32 @@
   <!-- CONTACT SECTION -->
   <div id="contact" class="w3-content w3-container w3-padding-64">
 
-  <div class="w3-card-3 w3-hover-shadow w3-dark-grey w3-padding-large">
-    <h3 class="w3-center">Contact Me!</h1>
-      <p class="w3-center"><em>I'd love your feedback!</em></p>
-      <div class="w3-center">
-        <div class="w3-row">
-          <div class="w3-half right-box">
-            <i class="fa fa-map-marker fa-lg icon-spacing" aria-hidden="true"></i>
-            <p class="contact-info"> Spokane, Washington</p>
-          </div>
-          <div class="w3-half left-box">
-            <i class="fa fa-envelope fa-lg icon-spacing" aria-hidden="true"></i>
-            <p class="contact-info"> vaphotography@gmail.com</p>
+    <div class="w3-card-3 w3-hover-shadow w3-padding-large bg-grey">
+      <h3 class="w3-center">Contact Me!</h1>
+        <p class="w3-center"><em>I'd love your feedback!</em></p>
+        <div class="w3-center">
+          <div class="w3-row">
+            <div class="w3-half right-box">
+              <i class="fa fa-map-marker fa-lg icon-spacing" aria-hidden="true"></i>
+              <p class="contact-info"> Spokane, Washington</p>
+            </div>
+            <div class="w3-half left-box">
+              <i class="fa fa-envelope fa-lg icon-spacing" aria-hidden="true"></i>
+              <p class="contact-info"> vaphotography@gmail.com</p>
+            </div>
           </div>
         </div>
-      </div>
-  </div>
+    </div>
   </div>
 
   <!-- INCLUDE FOOTER -->
-  <?php include 'modules/footer.php'; ?>
+  <footer class="w3-center w3-padding-64 bg-grey">
+    <div class="w3-xlarge w3-section"></div>
+    <p>
+      &copy VAPhotography 
+      <script>document.write(new Date().getFullYear())</script>
+    </p>
+  </footer>
 
   <script src="js/main.js"></script>
 </body>
