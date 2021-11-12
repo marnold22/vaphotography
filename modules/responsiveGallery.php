e92112abe4d0b6cba589fd25b6935eddde6c0b2b
Michael <michaelarnold615@gmail.com>
2021-11-12 13:01:34 -0800

    updated responsiveGallery to iterate through all images

--- modules/responsiveGallery.php
+++ modules/responsiveGallery.php
@@ -1,28 +1,10 @@
 <div id="gallery">
 
-    <img src="assets/gallery2/(1).jpg">
-    <img src="assets/gallery2/(2).jpg">
-    <img src="assets/gallery2/(3).jpg">
-    <img src="assets/gallery2/(4).jpg">
-    <img src="assets/gallery2/(5).jpg">
-    <img src="assets/gallery2/(6).jpg">
-    <img src="assets/gallery2/(7).jpg">
-    <img src="assets/gallery2/(8).jpg">
-    <img src="assets/gallery2/(9).jpg">
-    <img src="assets/gallery2/(10).jpg">
-    <img src="assets/gallery2/(11).jpg">
-    <img src="assets/gallery2/(12).jpg">
-    <img src="assets/gallery2/(13).jpg">
-    <img src="assets/gallery2/(14).jpg">
-    <img src="assets/gallery2/(15).jpg">
-    <img src="assets/gallery2/(16).jpg">
-    <img src="assets/gallery2/(17).jpg">
-    <img src="assets/gallery2/(18).jpg">
-    <img src="assets/gallery2/(19).jpg">
-    <img src="assets/gallery2/(20).jpg">
-    <img src="assets/gallery2/(21).jpg">
-    <img src="assets/gallery2/(22).jpg">
-    <img src="assets/gallery2/(23).jpg">
-    <img src="assets/gallery2/(24).jpg">
+    <?php
+        for ($i=0; $i <= 195; $i++) {
+            echo("<img src='assets/gallery/($i).jpg'>");
+        }
+    
+    ?>
 
 </div>
