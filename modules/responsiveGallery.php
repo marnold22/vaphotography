741d320fd87f136979ff453e5d92dec269c92e44
Michael <michaelarnold615@gmail.com>
2021-11-12 07:57:20 -0800

    Created a new responsive gallery page (temp) and corresponding css

---
+++ modules/responsiveGallery.php
@@ -0,0 +1,28 @@
+<div id="gallery">
+
+    <img src="assets/gallery/image-01.jpg">
+    <img src="assets/gallery/image-02.jpg">
+    <img src="assets/gallery/image-03.jpg">
+    <img src="assets/gallery/image-04.jpg">
+    <img src="assets/gallery/image-05.jpg">
+    <img src="assets/gallery/image-06.jpg">
+    <img src="assets/gallery/image-07.jpg">
+    <img src="assets/gallery/image-08.jpg">
+    <img src="assets/gallery/image-09.jpg">
+    <img src="assets/gallery/image-10.jpg">
+    <img src="assets/gallery/image-11.jpg">
+    <img src="assets/gallery/image-12.jpg">
+    <img src="assets/gallery/image-13.jpg">
+    <img src="assets/gallery/image-14.jpg">
+    <img src="assets/gallery/image-15.jpg">
+    <img src="assets/gallery/image-16.jpg">
+    <img src="assets/gallery/image-17.jpg">
+    <img src="assets/gallery/image-18.jpg">
+    <img src="assets/gallery/image-19.jpg">
+    <img src="assets/gallery/image-20.jpg">
+    <img src="assets/gallery/image-21.jpg">
+    <img src="assets/gallery/image-22.jpg">
+    <img src="assets/gallery/image-23.jpg">
+    <img src="assets/gallery/image-24.jpg">
+
+</div>
