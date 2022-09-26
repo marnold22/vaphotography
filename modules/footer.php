c7d214dd26b6885aa4edf9119c0e185ede09c37e
Michael <michaelarnold615@gmail.com>
2022-09-26 15:26:35 -0700

    Removed Modules folder

--- modules/footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="w3-center w3-padding-64 bg-grey">
-    <div class="w3-xlarge w3-section"></div>
-    <p>
-        &copy VAPhotography
-        <script>
-            document.write(new Date().getFullYear())
-        </script>
-    </p>
-</footer>
