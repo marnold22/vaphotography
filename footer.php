6f7fc5ff10e4dcbfdac4d20d33fb16fc58dd414c
Michael <michaelarnold615@gmail.com>
2022-09-26 15:26:57 -0700

    Moved footer & header to root directory

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer class="w3-center w3-padding-64 bg-grey">
+    <p class="contact-text">
+      &copy VAPhotography
+      <script>
+        document.write(new Date().getFullYear())
+      </script>
+    </p>
+
+</footer>
