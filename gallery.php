732e00d7adbcb5b50e5b87e85dace614746c13c3
Michael <michaelarnold615@gmail.com>
2022-09-28 11:42:10 -0700

    Changed gallery structure
    Using 4 columns, calculate number of rows and loop through laying out the cards/images in a grid

--- gallery.php
+++ gallery.php
@@ -37,24 +37,36 @@
     <p class="w3-center"><em>Here are some of my latest pieces of work!<br> Click on the images to make them larger</em>
     </p><br>
 
-    <div id="gallery">
-      <!-- <ul class="image-gallery"> -->
-      <?php
-        // count the number of pictures in the gallery folder
-        // Then iterate through creating an image tag for each picture
-        // Start the counting at the max number of pictures and descende that way newest images are on top of the gallery
+    <?php
+    // count the number of pictures in the gallery folder
+    $num_pics = count(glob("assets/gallery/*.jpg")) - 1;
 
-        $i = count(glob("assets/gallery/*.jpg"))-1;
-        while( $i >= 0 ) {
-          echo
-            (
-              "<img src='assets/gallery/($i).jpg' onclick='galleryDisplay(this)'>"
-            );
-            $i--;
+    // Loop through and make the cards
+    $pic_count = $num_pics;
+    $columns = 4;
+    $rows = ceil($num_pics / $columns);
+
+    for ($i = 0; $i <= $rows; $i++) {
+      echo ('<div class="w3-row-padding w3-margin-top">');
+      for ($j = 0; $j < 4; $j++) {
+        if ($pic_count >= 0) {
+          echo ("
+            <div class='w3-quarter w3-margin-top'>
+            <div class='w3-card cardhover'>
+            <img class='imgcard' src='assets/gallery/(" . $pic_count . ").jpg' onclick='galleryDisplay(this)'>
+            <div class='w3-container'>
+            </div>
+            </div>
+            </div>
+          ");
+          $pic_count = $pic_count - 1;
+        } else {
+          break;
         }
-      ?>
-      <!-- </ul> -->
-    </div>
+      }
+      echo ('</div>');
+    } ?>
+    
   </div>
   <!-- GALLERY END -->
 
